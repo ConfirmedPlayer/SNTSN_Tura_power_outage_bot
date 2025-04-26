57d96d9c59adf8afdca54d34ee8a2e67e360f6b5
ConfirmedPlayer <confirmedplayer@mail.ru>
2025-04-26 15:44:15 +0500

    moved telegram logic to certain file

---
+++ src/telegram.py
@@ -0,0 +1,20 @@
+from aiogram.types import Message
+from config import bot
+import env
+from loguru import logger
+
+
+@logger.catch
+async def edit_message_in_channel(text: str) -> Message | bool:
+    return await bot.edit_message_text(
+        text=text,
+        chat_id=env.TELEGRAM_BOT_CHANNEL_ID,
+        message_id=env.TELEGRAM_BOT_MESSAGE_ID,
+    )
+
+
+@logger.catch
+async def send_message_in_channel(text: str) -> Message:
+    return await bot.send_message(
+        chat_id=env.TELEGRAM_BOT_CHANNEL_ID, text=text
+    )
